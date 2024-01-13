779409a547d8fd3229a300e31744971dbbb1e7a0
Marco-Silvera <marksilver_1_0@hotmail.com>
2024-01-12 21:45:18 -0500

    Home terminado

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+
+// Header fixed
+
+window.addEventListener("scroll", function () {
+    var header = document.querySelector("header");
+    header.classList.toggle("abajo", window.scrollY > 0);
+})
+
+
+// Menu responsive
+
+function showSidebar() {
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = 'none'
+}
+
+
 document.querySelectorAll('a.nav-link').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -7,3 +29,4 @@ document.querySelectorAll('a.nav-link').forEach(anchor => {
         });
     });
 });
+
